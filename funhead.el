313acf505a85defb5920db593f372a9610598940
Dave Pearson <davep@davep.org>
2019-01-23 09:16:36 +0000

    :sparkles: Add stylesheets

--- funhead.el
+++ funhead.el
@@ -33,7 +33,7 @@
   (cond ((derived-mode-p 'python-mode 'sh-mode)
          (lambda ()
            (insert (concat (make-string 78 ?#) "\n# "))))
-        ((derived-mode-p 'js-mode)
+        ((derived-mode-p 'js-mode 'css-mode)
          (lambda ()
            (insert "/**\n * \n */")
            (next-line -1)))))
