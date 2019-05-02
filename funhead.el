9b2cfbf734de7a03ed82f9a79ca3ce8466eaa25e
Dave Pearson <davep@davep.org>
2019-05-02 13:07:44 +0100

    :sparkles: Add support for perl
    I know. I know.

--- funhead.el
+++ funhead.el
@@ -30,7 +30,8 @@
 
 (defun funhead--format ()
   "Return the correct `funhead' format for the current buffer."
-  (cond ((derived-mode-p 'python-mode 'sh-mode 'makefile-gmake-mode 'yaml-mode)
+  (cond ((derived-mode-p 'python-mode 'sh-mode 'makefile-gmake-mode
+                         'yaml-mode 'perl-mode)
          (lambda ()
            (insert (concat (make-string 78 ?#) "\n# "))))
         ((derived-mode-p 'emacs-lisp-mode 'lisp-mode)
