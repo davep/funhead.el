68127d479861bde3809fa23c9b986c33e9e28b88
Dave Pearson <davep@davep.org>
2019-05-03 09:17:54 +0100

    :bookmark: v1.6

--- funhead.el
+++ funhead.el
@@ -2,7 +2,7 @@
 ;; Copyright 2019 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.5
+;; Version: 1.6
 ;; Keywords: convenience
 ;; URL: https://github.com/davep/funhead.el
 ;; Package-Requires: ((emacs "24"))
